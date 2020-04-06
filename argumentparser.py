13829778fcf8e691756e7e28b5abd1a9e8bbfd13
Nick Marshall <marshallnw18@gmail.com>
2020-04-06 18:15:23 -0400

    Added reversal logic to argumentsparser.py

--- argumentparser.py
+++ argumentparser.py
@@ -9,10 +9,15 @@ parser.add_argument('filename', help='the file to read')
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
 parser.add_argument('--version','-v', action='version', version='%(prog)s 1.0')
 
+#parse and print the arguments passed in by the user
 args = parser.parse_args()
-print(args) 
 
-#parse the arguments
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
 
-#read the file, reverse the contents, and print
-
+    if args.limit:
+        lines = lines[:args.limit]
+    
+    for line in lines:
+        print(lines.strip()[::1])
