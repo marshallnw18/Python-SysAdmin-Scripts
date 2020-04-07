895eef37919b2207713a39546697830d38098582
Nick Marshall <marshallnw18@gmail.com>
2020-04-07 00:14:39 -0400

    userinput.py updated

--- userinput.py
+++ userinput.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3.6
 
-name = input("What is your name? ")
-birthdate = input("What is your birth date? ")
-#cast to an integer because "input" will return a string
-age = int(input("How old are you? "))
+def get_file_name(reprompt=False):
+    if reprompt:
+        print("Please enter a file name: ")
+    
+    #take user input for the filename and then strip whitespaces
+    file_name = input("Destination file name: ").strip()
 
-print(f"Your name is {name}")
-print(f"You were born on {birthdate} and you are {age} years old.")
+    #if no filename is given, reprompt the user for a filename
+    return file_name or get_file_name(True)
 
-def age_add(age):
-    return(age + 5)
+file_name = get_file_name()
 
-print(f"In five years, you will be {age_add(age)} years old.")
+print(f"Please enter your content. Entering an empty line will write the content to {file_name}:\n")
 
+#open the given filename and begin writing to it
+with open(file_name, 'w') as f:
+    eof = False
+    lines = []
+
+    while not eof:
+        line = input()
+
+        #if a line is given as input, strip it and append to the list 'lines'
+        if line.strip():
+            lines.append(f"{line}\n")
+        #if no input is given, consider it to be the end of input 
+        else:
+            eof = True
+    
+    #write lines to the given file once input has completed
+    f.writelines(lines)
+    print(f"Lines written to {file_name}")
 
