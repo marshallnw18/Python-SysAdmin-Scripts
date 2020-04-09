361654d70eb812e745a18026b423902958b9f26a
Nick Marshall <marshallnw18@gmail.com>
2020-04-09 14:07:53 -0400

    Completed 8 additional python scripts

---
+++ learn-python-the-hard-way/ex11.py
@@ -0,0 +1,7 @@
+#!/bin/usr/env python3.6
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So you're {age} years old, {height} tall, and {weight} heavy.")
