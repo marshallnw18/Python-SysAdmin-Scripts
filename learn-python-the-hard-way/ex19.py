792916fae1de17e0cc157fd1ff8ee8c943544d04
Nick <marshallnw18@gmail.com>
2021-07-12 18:31:27 -0400

    Correcting spelling mistake

--- learn-python-the-hard-way/ex19.py
+++ learn-python-the-hard-way/ex19.py
@@ -4,7 +4,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {cheese_count} cheeses!")
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
-    print("Get a blankets.\n")
+    print("Get a blanket.\n")
 
 print("We can just give the function numbers directly:")
 cheese_and_crackers(20,30)
